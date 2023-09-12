2fb8c037e7e8d54b68d1a2b0886dd3a17354367b
Nyiriek <n.peat@alustudent.com>
2023-09-12 06:04:45 -0700

    Minimum Operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""method that calculates the fewest number of operations needed"""
+
+import math
+
+
+def minOperations(n):
+    """minimum operations to get n to 1"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
+    return operations
+
